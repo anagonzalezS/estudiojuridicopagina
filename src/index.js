3c4f8a01548cfcd685f66c4bbc7525539cf6ef9b
AbrahamOsco <anaester.gonzalez@live.com>
2024-03-29 22:37:10 -0300

    index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa el archivo CSS de Bootstrap
+import 'animate.css/animate.min.css'
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
