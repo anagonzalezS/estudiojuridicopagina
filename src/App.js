009f7328757d24bb718e1701c2186912a8be7fae
AbrahamOsco <anaester.gonzalez@live.com>
2024-03-29 21:18:02 -0300

    modificaciones

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import Home from './components/Home'; // Importa el componente Home desde su ubicación
+import Header from './components/Header';
+import Navbar from './components/principal/Navbar'; // Corregido el nombre del componente NavBar a Navbar
+import Home from './components/Home';
+import About from './components/About';
+import Service from './components/Service';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import WhatsappIcon from './components/WhatsappIcon'; // Importa el componente WhatsappIcon
 
-const App = () => {
+function App() {
   return (
     <div>
-      <Home /> {/* Renderiza el componente Home */}
+      <Header />
+      <Navbar /> {/* Cambiado a Navbar en lugar de NavBar */}
+      <Home />
+      <About />
+      <Service />
+      <Contact />
+      <Footer />
+      <WhatsappIcon /> {/* Agrega el ícono flotante WhatsappIcon aquí */}
     </div>
   );
 }
