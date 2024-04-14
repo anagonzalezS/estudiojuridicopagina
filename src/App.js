07f8535746577052c64e0773c0b26646b1a8558b
ani <anaester.gonzalez@live.com>
2024-04-14 13:26:15 -0300

    agrego archivo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Home from './components/Home'; // Importa el componente Home desde su ubicación
-
+import Header from './components/Header'
 const App = () => {
   return (
     <div>
+      <Header/>
+
       <Home /> {/* Renderiza el componente Home */}
     </div>
   );
