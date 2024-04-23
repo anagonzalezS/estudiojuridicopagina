8a80ec77f3995875ff50b74bf328f6d32d2a1213
AbrahamOsco <anaester.gonzalez@live.com>
2024-04-23 20:28:19 -0300

    retoques

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css'; // Importa tus estilos personalizados aquí
 import 'animate.css/animate.min.css'; // Importa la animación CSS si es necesario
 
 function Home() {
-  const textoVisible = "Haga su consulta Estudio Jurídico Saenz & Asociados";
+  const textoVisible = "Estudio Jurídico Saenz & Asociados";
   const [typedText, setTypedText] = useState('');
   const typingDelay = 100; // Delay entre cada letra
   const erasingDelay = 50; // Delay entre el borrado de letras
@@ -42,7 +42,9 @@ function Home() {
         <div className="position-absolute top-50 start-50 translate-middle text-center text-light">
           <div className="d-flex flex-column justify-content-center align-items-center" style={{ minHeight: '80vh', minWidth: '320px' }}> {/* Tamaño mínimo para evitar cambios bruscos */}
             {/* Contenedor centrado verticalmente */}
-            <h1 className="display-1 mb-4 wow fadeIn" style={{ fontSize: '52px', fontFamily: 'inherit', marginBottom: '0', minHeight: '52px' }}>SAENZ & ASOCIADOS</h1>
+            <h1 className="display-1 mb-4 wow fadeIn uppercase-text" style={{ fontSize: '52px', fontFamily: 'inherit', marginBottom: '0', minHeight: '52px' }}>Sáenz & Asociados</h1>
+
+
             <div className="typed-text-container" style={{ position: 'relative', width: '100%', textAlign: 'center', marginBottom: '20px', minHeight: '50px' }}> {/* Tamaño fijo para el contenedor */}
               <p className="lead" style={{ fontSize: '20px', margin: '0' }}>{typedText}</p> {/* Reducir el tamaño de la fuente */}
             </div>
--- src/components/principal/Navbar.jsx
+++ src/components/principal/Navbar.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
+import './Navbar.css'; // Asegúrate de tener este archivo CSS
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white" role="navigation">
-  <div className="container">
-    <a className="navbar-brand" href="#top">
-      <img src="/logoweb.png" alt="Logo" style={{ width: '140px', height: 'auto' }} />
-    </a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <a className="nav-link font-weight-bold" href="#top">INICIO</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link font-weight-bold" href="#about">NOSOTROS</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link font-weight-bold" href="#service">SERVICIOS</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link font-weight-bold" href="#turno">TURNO</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link font-weight-bold" href="#contact">CONTACTO</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
+      <div className="container">
+        <a className="navbar-brand" href="#top">
+          <img src="/logoweb.png" alt="Logo" style={{ width: '140px', height: 'auto' }} />
+        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-label">Menú</span> {/* Texto adicional para móviles */}
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link font-weight-bold" href="#top">INICIO</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link font-weight-bold" href="#about">NOSOTROS</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link font-weight-bold" href="#service">SERVICIOS</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link font-weight-bold" href="#turno">TURNO</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link font-weight-bold" href="#contact">CONTACTO</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
