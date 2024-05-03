41f0e856afc8c07bc356fd761b2268136e4ec5df
AbrahamOsco <anaester.gonzalez@live.com>
2024-05-02 22:22:57 -0300

    modifico color menu

--- src/components/principal/Navbar.jsx
+++ src/components/principal/Navbar.jsx
@@ -11,43 +11,32 @@ function Navbar() {
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
           <span className="navbar-toggler-label" style={{ marginLeft: '5px' }}>Menú</span>
-
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav">
-
-
+          <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link " href="#top">INICIO</a>
+              <a className="nav-link" href="#top">INICIO</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link " href="#about">NOSOTROS</a>
+              <a className="nav-link" href="#about">NOSOTROS</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link " href="#service">SERVICIOS</a>
+              <a className="nav-link" href="#service">SERVICIOS</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link " href="#turno">TURNO</a>
+              <a className="nav-link" href="#turno">TURNO</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link " href="#tarifa">TARIFA</a>
+              <a className="nav-link" href="#tarifa">TARIFA</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link " href="#planificacion">PLANIFICACION FINANCIERA</a>
+              <a className="nav-link" href="#planificacion">PLANIFICACION FINANCIERA</a>
             </li>
-            
-            
           </ul>
         </div>
       </div>
     </nav>
   );
 }
-// Agregar el código JavaScript al final del componente Navbar
-document.addEventListener("DOMContentLoaded", function () {
-  // Activar el menú desplegable al hacer clic en el botón del menú
-  document.querySelector(".navbar-toggler").addEventListener("click", function () {
-    document.querySelector("#navbarSupportedContent").classList.toggle("show");
-  });
-});
+
 export default Navbar;
