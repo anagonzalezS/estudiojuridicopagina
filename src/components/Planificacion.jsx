6881a39c822b4a43a2beff14066962e36b700026
ana <anaester.gonzalez@live.com>
2024-08-27 18:16:19 -0300

    planificacion

--- src/components/Planificacion.jsx
+++ src/components/Planificacion.jsx
@@ -21,7 +21,7 @@ function Planificacion() {
       <section id="planificacion" className="container mt-5">
         <div className="row justify-content-center">
           <div className="col-md-12">
-            <h2 className="text-center mb-4">Administración Financiera</h2>
+            <h2 className="text-center mb-4">Planificacion Financiera</h2>
           </div>
         </div>
         <div className="row justify-content-center">
