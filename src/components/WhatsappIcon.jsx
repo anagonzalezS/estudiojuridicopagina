81eed087a33a18b79e6d99a02ca9cda51d102d91
Ana <anaester.gonzalez@live.com>
2025-09-07 19:33:17 -0300

    edito icono

--- src/components/WhatsappIcon.jsx
+++ src/components/WhatsappIcon.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import { FaWhatsapp } from 'react-icons/fa';
+import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
+import "./WhatsappIcon.css"; // Importamos los estilos
 
 const WhatsappIcon = () => {
-    const phoneNumber = "+5491162087349"; // Reemplaza con tu número de teléfono, incluyendo el código de país
-    const message = encodeURIComponent("Hola, ¡quiero que se contacten conmigo! ");
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    let whatsappLink;
+  const phoneNumber = "+5491162087349"; // Número con código de país
+  const message = encodeURIComponent("Hola, ¡quiero que se contacten conmigo! ");
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-    if (isMobile) {
-        whatsappLink = `https://wa.me/${phoneNumber}?text=${message}`;
-    } else {
-        whatsappLink = `https://web.whatsapp.com/send?phone=${phoneNumber}`;
-    }
+  const whatsappLink = isMobile
+    ? `https://wa.me/${phoneNumber}?text=${message}`
+    : `https://web.whatsapp.com/send?phone=${phoneNumber}&text=${message}`;
 
-    return (
-        <a href={whatsappLink} className="whatsapp-float" title="Saenz & Asociados">
-            <FaWhatsapp size={50} color="#fff" />
-        </a>
-    );
-}
+  return (
+    <a
+      href={whatsappLink}
+      className="whatsapp-float"
+      target="_blank"
+      rel="noopener noreferrer"
+      title="Saenz & Asociados"
+    >
+      <FaWhatsapp className="whatsapp-icon" />
+    </a>
+  );
+};
 
 export default WhatsappIcon;
